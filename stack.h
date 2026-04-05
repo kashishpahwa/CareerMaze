59f923c91fd00c6e3e49aea64c90969038fac727
kashishpahwa <kashishpahwa618@gmail.com>
2026-04-05 15:28:58 +0530

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,29 +1,25 @@
-#ifndef STACK_H
-#define STACK_H
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct stack {
-    void *ptr;
+    void  *ptr;
     struct stack *next;
 };
 typedef struct stack stack;
 
-void push(stack **top,void *p){
-    stack *t=(stack*)malloc(sizeof(stack));
-    t->ptr=p;
-    t->next=*top;
-    *top=t;
+void push(stack **top, void *p){
+    stack *t = (stack *)malloc(sizeof(stack));
+    t->ptr= p;
+    t->next= *top;
+    *top = t;
 }
 
 void* pop(stack **top){
-    if(*top==NULL) return NULL;
-
-    stack *t=*top;
-    void *p=t->ptr;
-    *top=t->next;
+    if(*top == NULL) return NULL;
+    stack *t = *top;
+    void  *p= t->ptr;
+    *top = t->next;
     free(t);
     return p;
 }
 
-#endif
